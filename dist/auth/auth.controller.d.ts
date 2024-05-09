ae1d5040dcd4f9aa018e894c995fb123b1d36b52
Desplay Shido <101574256+Desplay@users.noreply.github.com>
2024-05-09 11:21:53 +0700

    update .gitignore

--- dist/auth/auth.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AuthService } from './auth.service';
-import { UserSignIn } from 'src/common/datatype/dto/auth.dto';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    login(userSignIn: UserSignIn): string;
-}
