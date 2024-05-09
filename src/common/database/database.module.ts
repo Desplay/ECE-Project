7f9a2e6623da152a54224a3980edee74621bfff6
Desplay Shido <101574256+Desplay@users.noreply.github.com>
2024-05-09 11:32:30 +0700

    update mongo module

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -4,7 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      process.env.MONGODB_URI ||
+      process.env.DATABASE_URL ||
+        process.env.MONGODB_URI ||
         process.env.LOCAL_MONGODB_URI ||
         'mongodb://localhost:27017/ECE-Project',
     ),
